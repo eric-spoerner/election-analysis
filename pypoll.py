650726c7776db683db48714a69d8d974e5ff1972
Eric Spoerner <espoerner@gmail.com>
2021-12-02 08:39:36 -0800

    Update to include algo and printing for county-by-county vote segmentation.

--- pypoll.py
+++ pypoll.py
@@ -10,6 +10,9 @@
     # Percentage votes from each county out of total count
     # County with highest turnout
 
+# Extra credit:
+    # Candidate breakdown per county
+
 import csv
 import os
 
@@ -24,6 +27,12 @@ candidate_vote = dict()
 county_options = list()
 county_vote = dict()
 
+# BEGIN TEST DEBUG CODE TO ADD FULL CANDIDATE LIST
+#temporary code for new algo, create new list for agg vote
+
+all_counties = {}
+
+# Commenting all of the below out for the moment
 with open(votes_file) as election_data:
     file_reader = csv.reader(election_data)
 
@@ -38,27 +47,46 @@ with open(votes_file) as election_data:
         county_name = row[1]
         candidate_name = row[2]
 
-        if not(candidate_name in candidate_options): 
-            # Add new county if not currently in list and start vote tracker
-            candidate_options.append(candidate_name)
+        # Add missing county to county list
+        if not(county_name in all_counties.keys()):
+            all_counties[county_name] = {} # init dict entry with new county
+            county_options.append(county_name) # add county name to reference list
+            county_vote[county_name] = 0 # add to dict for tracking aggregate county stats
+
+            for name in candidate_options: # add all known candidates from list to new county
+                all_counties[county_name][name] = 0
+                
+        if not(candidate_name in candidate_options):
+            candidate_options.append(candidate_name) # add candidate to reference list
             candidate_vote[candidate_name] = 0
 
-        if not (county_name in county_options):
-            # Add new candidate if not currently in list and start vote tracker
-            county_options.append(county_name)
-            county_vote[county_name] = 0
+            #init name in dict --- iterate over all existing counties and add candidate at 0
+            for county in all_counties.keys():
+                all_counties[county].update({candidate_name:0})
 
-        # Add votes to candidate tallies
+        # Add vote count to county/candidate segmentation analysis
+        new_vote_count = all_counties[county_name][candidate_name] + 1
+        all_counties[county_name].update({candidate_name : new_vote_count})
+
+        # Add vote count to one dimensional data structures
         candidate_vote[candidate_name] += 1
         county_vote[county_name] += 1
+
+
+# Calculate total votes per county and add to dictionary as new kvp
+# for county in all_counties:
+#     county_total = 0
+
+
+divider_line = f"--------------------------\n"
 
 # Write total results to output file.
 with open(output_path, "w") as txt_file:
     election_results = (
-        f"Election Results\n"
-        f"--------------------------\n"
-        f"Total votes: {totalVotes:,}\n"
-        f"--------------------------\n"
+        f"Election Results\n" +
+        divider_line +
+        f"Total votes: {totalVotes:,}\n" +
+        divider_line
     )
     print(election_results, end="")
 
@@ -86,14 +114,14 @@ with open(output_path, "w") as txt_file:
             winning_county = county
             winning_count = county_vote_total
 
-    txt_file.write(f"--------------------------\n")
-    print(f"--------------------------\n")
+    txt_file.write(divider_line)
+    print(divider_line)
     
     winning_county_summary = f"Largest County Turnout: {winning_county}\n"
     print(winning_county_summary)
     txt_file.write(winning_county_summary)
-    txt_file.write(f"--------------------------\n")
-    print(f"--------------------------\n")
+    txt_file.write(divider_line)
+    print(divider_line)
 
     # Calculate percentages for each county and determine a winner
     winning_candidate = ""
@@ -116,8 +144,41 @@ with open(output_path, "w") as txt_file:
             winning_count = candidate_vote_total
             winning_percentage = vote_percent
 
-    print(f"--------------------------\n")
-    txt_file.write(f"--------------------------\n")
+    print(divider_line)
+    txt_file.write(divider_line)
     winning_candidate_summary = f"Winner: {winning_candidate}\nWinning Vote Count: {winning_count:,}\nWinning Percentage: {winning_percentage:.1f}%"
     print(winning_candidate_summary)
     txt_file.write(winning_candidate_summary)
+
+
+    county_result_output = ("\n\n" + 
+                            divider_line +
+                            f"VOTE BREAKDOWN BY COUNTY\n" +
+                            divider_line
+    )
+
+    # Tally and publish individual vote counts
+    for county in all_counties:
+
+        county_result_output += f"County: {county}\n"
+        total_county_votes = 0
+        highest_candidate_county_vote_count = 0
+        highest_county_candidate = ""
+        # candidate_county_percentage = 0.0
+
+        for candidate in all_counties[county]:
+            num_votes = all_counties[county][candidate]
+
+            if num_votes > total_county_votes:
+                highest_candidate_county_vote_count = num_votes
+                highest_county_candidate = candidate
+
+            total_county_votes += num_votes
+            county_result_output += f"\t{candidate}: {num_votes:,}\n"
+
+        county_result_output += f"Total votes in county: {total_county_votes:,}\n"
+        county_result_output += f"Candidate with most votes received in {county}: {highest_county_candidate}\n"
+        county_result_output += divider_line
+
+    print(county_result_output)
+    txt_file.write(county_result_output)
